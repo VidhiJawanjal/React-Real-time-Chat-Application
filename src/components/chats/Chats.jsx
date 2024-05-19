dc25a0ce05b79a04c01559372d59386ba3bcc03a
vidhi jawanjal <minaljawanjal123@gmai.com>
2024-05-19 11:23:22 +0530

    posting code

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./chats.css";
 import EmojiPicker from "emoji-picker-react";
 
 const Chats = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState(false);
+
+  const endRef = useRef(null);
+
+  useEffect(()=>{
+    endRef.current?.scrollIntoView({behavior:"smooth"})
+  },[])
 
   const handleEmoji = (e) => {
     console.log(e);
@@ -14,6 +20,7 @@ const Chats = () => {
 
   return (
     <div className="chats">
+          {/* ............... top ............... */}
       <div className="top">
         <div className="userInfo">
           <img
@@ -34,7 +41,53 @@ const Chats = () => {
           <i className="fa-solid fa-info"></i>
         </div>
       </div>
-      <div className="center"></div>
+      {/* ............... center ............... */}
+      <div className="center">
+        <div className="message">
+          
+
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, praesentium dolorum neque vitae ratione facere deserunt? Necessitatibus, nisi. Ut, consequuntur?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message owner">
+          
+
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, praesentium dolorum neque vitae ratione facere deserunt? Necessitatibus, nisi. Ut, consequuntur?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          
+
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, praesentium dolorum neque vitae ratione facere deserunt? Necessitatibus, nisi. Ut, consequuntur?
+              
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message owner">
+
+          <div className="texts">
+              <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?size=626&ext=jpg&ga=GA1.1.1369675164.1715817600&semt=ais_user" alt="" />
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, praesentium dolorum neque vitae ratione facere deserunt? Necessitatibus, nisi. Ut, consequuntur?Lorem ipsum dolor sit amet qwer
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
+      {/* ............... bottom ............... */}
       <div className="bottom">
         <div className="icons">
           <i className="fa-solid fa-image"></i>
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
-import "./details.css"
-
+import React from "react";
+import "./details.css";
 
 const Details = () => {
   return (
-    <div className='details'>Details</div>
-  )
-}
+    <div className="details">
+      <div className="user">
+        <img
+          src="https://imgv3.fotor.com/images/slider-image/A-blurry-close-up-photo-of-a-woman.jpg"
+          alt=""
+        />
+        <h2>Vidhi Jawanjal</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, eius
+          laboriosam placeat in consequatur quam.
+        </p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <i class="fa-solid fa-angle-up"></i>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy % help</span>
+            <i class="fa-solid fa-angle-up"></i>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <i class="fa-solid fa-angle-down"></i>
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://huggingface.co/datasets/huggingfacejs/tasks/resolve/main/image-classification/image-classification-input.jpeg"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <i class="fa-solid fa-download"></i>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://huggingface.co/datasets/huggingfacejs/tasks/resolve/main/image-classification/image-classification-input.jpeg"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <i class="fa-solid fa-download"></i>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://huggingface.co/datasets/huggingfacejs/tasks/resolve/main/image-classification/image-classification-input.jpeg"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <i class="fa-solid fa-download"></i>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://huggingface.co/datasets/huggingfacejs/tasks/resolve/main/image-classification/image-classification-input.jpeg"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <i class="fa-solid fa-download"></i>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <i class="fa-solid fa-angle-up"></i>
+          </div>
+        </div>
 
-export default Details
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
